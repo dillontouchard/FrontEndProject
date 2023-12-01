adabe7e15b7e0fac8b16698e1096df26f1445590
dillontouchard <dillon.touchard@yahoo.com>
2023-12-01 10:13:44 -0800

    Final Product

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 var playerID;
 let page = 1;
 let allStats = [];
+
+// Click Event for the Player search
+
 $('.btn.btn-outline-success').on('click', function(event){
     event.preventDefault()
     let input = $('input').val()
-    
+    if (input === "") {
+        alert('Make sure you enter a name!')
+        return;
+    }
+
+    // AJAX request 
 
     $.get(`https://www.balldontlie.io/api/v1/players?search=${input}`, function(data){
         console.log(data)
@@ -21,7 +29,7 @@ $('.btn.btn-outline-success').on('click', function(event){
             const playerTeam = players[i].team.full_name
             const playerHeightAndPosition = players[i].height_feet + "' " + players[i].height_inches + '" ' + players[i].position
             
-            var $playerCard = $('<span></span>').addClass('player-card');
+            var $playerCard = $('<span></span>').addClass('player-card')
             $results.append($playerCard)
             const $playerName = $('<h3></h3>').addClass('player-name').text(playerFName + " " + playerLName)
             $playerCard.append($playerName)
@@ -52,10 +60,21 @@ $('.btn.btn-outline-success').on('click', function(event){
         }
         fetchStats();
         
+        
+        
         var playerPoints = 0
         var playerAssists = 0
         var playerRebounds = 0
         var playerSteals = 0
+        
+        // Use conditional to present a "loading screen" in the player card 
+
+        if (playerPoints === 0) {
+            var $loadingText = $('<p></p>').text('Loading Player stats...')
+            $playerCard.append($loadingText)
+        }
+        
+        // Set Timeout to allow all stats to populate
         
         setTimeout(function() { 
             for(let i = 0; i < allStats.length; i++) {
@@ -64,8 +83,37 @@ $('.btn.btn-outline-success').on('click', function(event){
                 playerRebounds += allStats[i].reb
                 playerSteals += allStats[i].stl
             }
+            $loadingText.remove()
             const $playerStatOverview = $('<p></p>').addClass('stat-summary').html(`Career Points: ${playerPoints}<br>Career Assists: ${playerAssists}<br>Career Rebounds: ${playerRebounds}<br>Career Steals: ${playerSteals}`)
                 $playerCard.append($playerStatOverview)
         }, 7000)    
+    })
+})
+
+// Click Event for the Team Search 
+
+$('.btn1.btn-outline-success').on('click', function(event){
+    event.preventDefault()
+    let teamSearch = $('.team-search').val()
+    if (teamSearch === "") {
+        alert('Make sure you enter a team!')
+        return;
+    }
+    $.get(`https://www.balldontlie.io/api/v1/teams?page=1&per_page=45`, function(data) {
+        console.log(data)
+        $('#team').empty()
+        const teamArr = data.data
+        const $team = $('#team')
+        for (i = 0; i < teamArr.length; i++) {
+            if (teamArr[i].full_name === teamSearch) {
+                const $teamCard = $('<span></span>').addClass('team-card')
+                $team.append($teamCard)
+                const $teamName = $('<h3></h3>').addClass('team-name').text(`${teamArr[i].full_name} / ${teamArr[i].abbreviation}`)
+                $teamCard.append($teamName)
+                const $teamConfDiv = $('<h4></h4>').addClass('team-conf&div').html(`Team Conference: ${teamArr[i].conference}<br> Team Division: ${teamArr[i].division}`)
+                $teamCard.append($teamConfDiv)
+                
+            }
+        }
     })
 })
